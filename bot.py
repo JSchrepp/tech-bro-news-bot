b58e664779334df4770d9e36006de9eb5ba0d2db
James <jschrepp2121@gmail.com>
2022-05-05 16:32:04 -0700

    hate my life

---
+++ bot.py
@@ -0,0 +1,20 @@
+import tweepy
+import os
+
+from grammar import execute
+
+consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
+consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
+access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
+access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
+
+client = tweepy.Client(
+   consumer_key=consumer_key,
+   consumer_secret=consumer_secret,
+   access_token=access_token,
+   access_token_secret=access_token_secret
+)
+
+response = client.create_tweet(text='Hello World!')
+
+print(response)
