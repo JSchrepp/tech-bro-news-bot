69724658b86fce0c98b7eb7663529f8334acec2a
James <jschrepp2121@gmail.com>
2022-05-05 17:34:17 -0700

    refactor bot.py

--- bot.py
+++ bot.py
@@ -8,13 +8,12 @@ consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
 access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
 access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
 
-client = tweepy.Client(
-   consumer_key=consumer_key,
-   consumer_secret=consumer_secret,
-   access_token=access_token,
-   access_token_secret=access_token_secret
-)
+def send_tweet():
+   client = tweepy.Client(
+      consumer_key=consumer_key,
+      consumer_secret=consumer_secret,
+      access_token=access_token,
+      access_token_secret=access_token_secret
+   )
 
-response = client.create_tweet(text='Hello World!')
-
-print(response)
+   response = client.create_tweet(text=execute())
