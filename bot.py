7ebff744c0a250354cf8c970d5ed3599cbe41ad5
James <jschrepp2121@gmail.com>
2022-07-06 17:37:48 -0400

    refactor credential loading
    envvar => input json

--- bot.py
+++ bot.py
@@ -1,21 +1,22 @@
 import tweepy
+
+import json
 import os
+import sys
 
 from grammar import execute
 
-consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
-consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
-access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
-access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
-
-def send_tweet():
+def send_tweet(credential: dict):
    client = tweepy.Client(
-      consumer_key=consumer_key,
-      consumer_secret=consumer_secret,
-      access_token=access_token,
-      access_token_secret=access_token_secret
+      consumer_key=credential['consumer_key'],
+      consumer_secret=credential['consumer_secret'],
+      access_token=credential['access_token'],
+      access_token_secret=credential['access_token_secret']
    )
 
    response = client.create_tweet(text=execute())
 
-send_tweet()
+with open(sys.argv[1]) as raw_cred:
+   cred = json.load(raw_cred)
+
+send_tweet(cred)
